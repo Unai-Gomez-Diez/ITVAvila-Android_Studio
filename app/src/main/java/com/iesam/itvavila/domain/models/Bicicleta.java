19025c9060dd27e849a841f2687707232e685a2a
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-01-12 18:54:33 +0100

    Usando constructores

---
+++ app/src/main/java/com/iesam/itvavila/domain/models/Bicicleta.java
@@ -0,0 +1,17 @@
+package com.iesam.itvavila.domain.models;
+
+public class Bicicleta {
+    private String param1;
+    private String param2;
+
+    public Bicicleta(){
+
+    }
+    public Bicicleta(String param1){
+
+    }
+
+    public Bicicleta(String param1, String param2){
+
+    }
+}
